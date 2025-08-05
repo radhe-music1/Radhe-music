80949cd0e2c492354c23114760cd9fd543710a81
sachinopboy2 <sachin25u7@gmail.com>
2025-08-05 13:54:08 +0530

    Update settings.py

--- ShrutixMusic/plugins/bot/settings.py
+++ ShrutixMusic/plugins/bot/settings.py
@@ -189,7 +189,7 @@ async def send_repo(_, message: Message):
         caption=(
             "<b>✨ ʜᴇʏ ᴅᴇᴀʀ, ʜᴇʀᴇ ɪꜱ ᴛʜᴇ ᴏꜰꜰɪᴄɪᴀʟ ʀᴇᴘᴏꜱɪᴛᴏʀʏ ᴏꜰ ᴛʜɪꜱ ʙᴏᴛ ✨</b>\n\n"
             "🔗 ᴅᴏɴ'ᴛ ꜰᴏʀɢᴇᴛ ᴛᴏ ɢɪᴠᴇ ᴀ ꜱᴛᴀʀ 🌟 ᴀɴᴅ ꜰᴏʟʟᴏᴡ!\n\n"
-            "🧡 ᴄʀᴇᴅɪᴛꜱ : <a href='https://t.me/ShrutiBots'>@ShrutiBots</a>"
+            "🧡 ᴄʀᴇᴅɪᴛꜱ : <a href='https://t.me/COOL_NOBITA'>@COOL_NOBITA</a>"
         ),
         reply_markup=InlineKeyboardMarkup(
             [
@@ -412,4 +412,5 @@ async def vote_change(client, CallbackQuery, _):
         )
     except MessageNotModified:
         return
+
 
